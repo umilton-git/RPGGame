1b5b1509ba13cb1c83b931c3ac115a6008eab197
umilton-git <ulysses.github@protonmail.com>
2021-08-02 21:53:50 -0500

    Added movement system

---
+++ Assets/Components/TopDownCharacterController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    private InputHandler _input;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private Camera camera;
+
+    private void Awake()
+    {
+        _input = GetComponent<InputHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        
+        // Move in the direction we are aiming
+        var movementVector = MoveTowardTarget(targetVector);
+
+        //Rotate in the Direction we are travelling
+        RotateTowardMovementVector(movementVector);
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = moveSpeed * Time.deltaTime;
+
+        targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+
+        return targetVector;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementVector)
+    {
+        if(movementVector.magnitude == 0){ return; }
+        var rotation = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed);
+    }
+}
